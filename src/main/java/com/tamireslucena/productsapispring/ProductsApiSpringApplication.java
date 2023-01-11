55993e69739ce4a3ab6f7a348fb84accdc9d6bff
tamireslucena <tamiresb.lucena@gmail.com>
2023-01-11 18:18:53 -0300

    feat: create project

---
+++ src/main/java/com/tamireslucena/productsapispring/ProductsApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tamireslucena.productsapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamireslucena/productsapispring/ProductsApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamireslucena.productsapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
